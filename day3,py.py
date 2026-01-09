0b7999368bff24e692e8a3665a2982179fd6d0fb
navaneetha9391-glitch <navaneetha9391@gmail.com>
2026-01-09 22:06:30 +0530

    Add files via upload
    types of conversions and operators

---
+++ day3,py.py
@@ -0,0 +1,12 @@
+#simple calculator operations
+#variable and operation
+a=10
+b=20
+addition=a=b
+print("sum of two numbers is :",addition)
+print("subtraction of two numbers is :",a-b)
+print("muitipliction of two numbers is:",a*b)
+print("division of two numbers:",b/a)
+print("floor division of two  numbers:",b//a)
+print("modulo division of two numbers:",b%a)
+print("power of two numbers:",b**a)
